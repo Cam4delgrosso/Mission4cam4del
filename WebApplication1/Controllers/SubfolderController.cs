1cab055ddd43616b7d5277d9ea7b2474c2cfd545
Cameron Delgrosso <cam4del@byu.edu>
2023-02-06 10:28:02 -0700

    Mission 5 changes

---
+++ WebApplication1/Controllers/SubfolderController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class SubfolderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult GradeCalculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GradeCalculator(GradeCalculatorModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/GradeCalculatorModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    //Check to make sure that the inputs are between 0 and 100
+    public class GradeCalculatorModel
+    {
+ 
+        [Range(0, 100)]
+        public int Assignments { get; set; }
+        [Range(0, 100)]
+        public int Group { get; set; }
+        [Range(0, 100)]
+        public int Quizzes { get; set; }
+        [Range(0, 100)]
+        public int Midterm { get; set; }
+        [Range(0, 100)]
+        public int Final { get; set; }
+        [Range(0, 100)]
+        public int INTEX { get; set; }
+    }
+}
